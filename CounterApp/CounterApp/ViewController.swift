5b8b0567b95d17aa549233d7f389ad4d85a8d33a
Luca Pascucci <luca.pascucci@studio.unibo.it>
2014-10-31 19:58:18 +0100

    Finita CounterApp

--- CounterApp/CounterApp/ViewController.swift
+++ CounterApp/CounterApp/ViewController.swift
@@ -10,16 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var timer = NSTimer()
+    var counter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        labelCounter.text = String(counter)
     }
 
+    @IBOutlet var labelCounter: UILabel!
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
 
+    @IBAction func startCounterPressed(sender: AnyObject) {
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: ("update"), userInfo: nil, repeats: true)
+    }
+    
+    func update(){
+        labelCounter.text = String(counter++)
+    }
+    @IBAction func stopCounterPressed(sender: AnyObject) {
+        timer.invalidate()
+    }
+    @IBAction func clearPressed(sender: AnyObject) {
+        timer.invalidate()
+        counter = 0
+        labelCounter.text = String(counter)
+    }
 }
 
