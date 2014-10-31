12dd7c7561af1b6e4dcdf708ed6d3425517b4ace
Luca Pascucci <luca.pascucci@studio.unibo.it>
2014-10-31 17:07:53 +0100

    Creato Progetto CounterApp

---
+++ CounterApp/CounterApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CounterApp
+//
+//  Created by Luca Pascucci on 31/10/14.
+//  Copyright (c) 2014 Luca Pascucci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
