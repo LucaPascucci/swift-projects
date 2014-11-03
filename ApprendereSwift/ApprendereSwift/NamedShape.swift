5cb2b44cdc6dfbe6fe63f76f789e11eae5215868
Luca Pascucci <luca.pascucci@studio.unibo.it>
2014-11-03 18:35:35 +0100

    Aggiunto Progetto per apprendere SWIFT

---
+++ ApprendereSwift/ApprendereSwift/NamedShape.swift
@@ -0,0 +1,26 @@
+//
+//  NamedShape.swift
+//  ApprendereSwift
+//
+//  Created by Luca Pascucci on 03/11/14.
+//  Copyright (c) 2014 Luca Pascucci. All rights reserved.
+//
+
+import Foundation
+
+class NamedShape {
+    
+    var numberOfSides: Int = 0
+    
+    var name: String
+    
+    init(name: String){
+        
+        self.name = name
+    }
+    
+    func simpleDescription() -> String {
+        
+        return "A shape with \(numberOfSides) sides."
+    }
+}
---
+++ ApprendereSwift/ApprendereSwift/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  ApprendereSwift
+//
+//  Created by Luca Pascucci on 03/11/14.
+//  Copyright (c) 2014 Luca Pascucci. All rights reserved.
+//
+
+import Foundation
+
+let test = Square(sideLenght: 5.2, name: "My Square")
+
+
+println("A square with \(test.area()) of area")
+
+println(test.simpleDescription())
