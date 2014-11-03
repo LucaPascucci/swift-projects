5e6fdcbebf634ae218485c7eaa490830e34afb0a
Luca Pascucci <iMac@iMac-27.local>
2014-11-03 20:16:46 +0100

    Modifiche del Main

--- ApprendereSwift/ApprendereSwift/main.swift
+++ ApprendereSwift/ApprendereSwift/main.swift
@@ -8,9 +8,18 @@
 
 import Foundation
 
+// Variabile
+var myVariable = 42
+myVariable = 50
+
+// Costante
+let myConstant = 42
+
+let implicitInteger = 70
+let implicitDouble = 70.0
+let explicitDouble: Double = 70
+
+
 let test = Square(sideLenght: 5.2, name: "My Square")
-
-
 println("A square with \(test.area()) of area")
-
 println(test.simpleDescription())
